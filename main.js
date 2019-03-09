f1ee40deafb80c44f82b91de0b311bee9acca741
Jose <2648413936@qq.com>
2019-03-09 21:52:31 +0800

    解决切换页面时轮播出错的bug

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
- // 用状态机来理解轮播，现在包含三种状态，current、leave、stay。
+// 用状态机来理解轮播，现在包含三种状态，current、leave、stay。
 $(`.image:nth-child(1)`).addClass('current').siblings().addClass('stay')
 
 let n = 1;
 let imgLength = $('.image').length
-setInterval(() => {
-    becomeLeave(getNode(n)).one('transitionend', (e) => {
-        becomeStay(e.currentTarget)
-    })
-    becomeCurrent(getNode(n+1))
+let timer = setInterval(() => {
+  becomeLeave(getNode(n)).one('transitionend', (e) => {
+    becomeStay(e.currentTarget)
+  })
+  becomeCurrent(getNode(n + 1))
 }, n * 3000)
 
-
-
+// 当用户没有浏览当前页面时，浏览器的setInterval会出错，为了减小页面占用内存或cpu
+document.addEventListener("visibilitychange", function () {
+  if (document.hidden) {
+    window.clearInterval(timer)
+  } else {
+    console.log('dayui')
+    timer = setInterval(() => {
+      becomeLeave(getNode(n)).one('transitionend', (e) => {
+        becomeStay(e.currentTarget)
+      })
+      becomeCurrent(getNode(n + 1))
+    }, n * 3000)
+  }
+})
 
 
 
@@ -28,27 +40,27 @@ setInterval(() => {
 // 函数
 
 // 取得n
-function getChild(num){
-    let i = num % imgLength
-    if (i === 0) {
-        return n = 4
-    } else {
-        return n = i
-    }
+function getChild(num) {
+  let i = num % imgLength
+  if (i === 0) {
+    return n = 4
+  } else {
+    return n = i
+  }
 }
 
-function becomeCurrent(node){
-    return $(node).removeClass('stay').addClass('current')
+function becomeCurrent(node) {
+  return $(node).removeClass('stay').addClass('current')
 }
 
 function becomeLeave(node) {
-    return $(node).removeClass('current').addClass('leave')
+  return $(node).removeClass('current').addClass('leave')
 }
 
 function becomeStay(node) {
-    return $(node).removeClass('leave').addClass('stay')
+  return $(node).removeClass('leave').addClass('stay')
 }
 // 获取节点
-function getNode(n){
-    return `.image:nth-child(${getChild(n)})`
+function getNode(n) {
+  return `.image:nth-child(${getChild(n)})`
 }
